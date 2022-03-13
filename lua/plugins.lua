cb890c4a27f15238a43914b0e9fe17579df38d9b
xmirz <linearattack0@gmail.com>
2022-03-13 22:56:15 +0700

    added packer and vim-vinegar

---
+++ lua/plugins.lua
@@ -0,0 +1,6 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+
+  use 'tpope/vim-vinegar'
+end)
---
+++ lua/settings.lua
@@ -0,0 +1,8 @@
+HOME = os.getenv("HOME")
+
+-- WhiteSpace
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab =  true
