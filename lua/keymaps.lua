5d534b050f93c174f9934d6281b8fe8edd2a1482
xmirz <linearattack0@gmail.com>
2022-03-23 10:42:08 +0700

    added nvim tree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,8 +4,8 @@ local termOpts =  {silent = true}
 
   local keymap = vim.api.nvim_set_keymap
 
-vim.g.mapleader = ";"
-vim.g.localmapleader = ";"
+vim.g.mapleader = " "
+vim.g.localmapleader = " "
 
 -- Modes
 --   normal_mode = "n",
@@ -20,20 +20,25 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-Left>", "<C-w>h", opts)
+keymap("n", "<C-Down>", "<C-w>j", opts)
+keymap("n", "<C-Up>", "<C-w>k", opts)
+keymap("n", "<C-Right>", "<C-w>l", opts)
 
 
-keymap("","<C-b>", ":Lexplore 25<CR>", opts)
-keymap("i","<C-b>", "<ESC>:Lexplore 25<CR>", opts)
+-- NvimTree
+-- keymap("","<C-b>", ":Lexplore 25<CR>", opts)
+-- keymap("i","<C-b>", "<ESC>:Lexplore 25<CR>", opts)
+keymap("","<C-b>", "<ESC>:NvimTreeToggle<CR>", opts)
+keymap("","<Leader>e", "<ESC>:NvimTreeToggle<CR>", opts)
+keymap("i","<Leader>e", "<ESC>:NvimTreeToggle<CR>", opts)
 
--- Move text
-keymap("", "<A-Up>", ":m .-2<CR>==", opts)
-keymap("", "<A-Down>", ":m .+1<CR>==", opts)
 
 -- Resize windo
-keymap("", "<C-Up>", ":resize +2<CR>", opts)
-keymap("", "<C-Down>", ":resize -2<CR>", opts)
-keymap("", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("", "<C-A-Up>", ":resize +2<CR>", opts)
+keymap("", "<C-A-Down>", ":resize -2<CR>", opts)
+keymap("", "<C-A-Left>", ":vertical resize -2<CR>", opts)
+keymap("", "<C-A-Right>", ":vertical resize +2<CR>", opts)
 
 
 -- Buffer navigation
@@ -49,11 +54,17 @@ keymap("v","<", "<gv", opts)
 keymap("v", "p", '"_dP', opts)
 
 
--- Visual Block
 -- Move text
+keymap("", "<A-Up>", "<ESC>:m .-2<CR>==", opts)
+keymap("", "<A-Down>", ":<ESC>:m .+1<CR>==", opts)
 keymap("x", "<A-Up>", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-Down>", ":move '>+1<CR>gv-gv", opts)
 
+-- Copy text
+keymap("", "<A-S-Down>", ":copy .<CR>", opts)
+keymap("x", "<A-S-Down>", ":copy '><CR>", opts)
+
 -- Comment
 keymap("n", "<C-_>", "<CMD>lua require(\"Comment.api\").toggle_current_linewise_op()<CR>", opts)
+keymap("i", "<C-_>", "<CMD>lua require(\"Comment.api\").toggle_current_linewise_op()<CR>", opts)
 keymap("v", "<C-_>", "<ESC><CMD>lua require(\"Comment.api\").locked.toggle_linewise_op(vim.fn.visualmode())<CR>", opts)
